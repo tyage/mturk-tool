e71fc426d8fbc9b3d38bde80459c87885155c9ae
tyage <namatyage@gmail.com>
2015-09-30 13:29:05 +0900

    send request to mturk

--- app/services/mturk.js
+++ app/services/mturk.js
@@ -1,6 +1,34 @@
-class MTurk {
-  createHIT(hitTemplate) {
+import Config from './config';
+import crypto from 'crypto';
 
+let generateHmac = (data, key) => {
+  return crypto.createHmac('sha1', key).update(data).digest('base64');
+};
+
+// XXX: sandbox
+let endpoint = 'https://mechanicalturk.sandbox.amazonaws.com';
+
+class MTurk {
+  request(operation) {
+    let params = {
+      Service: 'AWSMechanicalTurkRequester',
+      AWSAccessKeyId: Config.get('AWSAccessKeyId'),
+      Version: '2014-08-15',
+      Operation: operation,
+      Timestamp: (new Date()).toISOString(),
+      HITTypeId: '',
+      Question: '',
+      LifetimeInSeconds: 604800
+    };
+    params.Signature = generateHmac(`${params.Service}${params.Operation}${params.Timestamp}`,
+      Config.get('AWSSecretAccessKey'));
+    let param = Object.keys(params).map((k) => `${k}=${encodeURIComponent(params[k])}`).join('&');
+
+    return fetch(`${endpoint}/?${param}`, {});
+  }
+  createHIT(hitTemplate, hitData) {
+    return this.request('CreateHIT');
   }
 };
+
 export default (new MTurk());
