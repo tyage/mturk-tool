220f64d7e3b7382cc1ef7574252938148016abdf
tyage <namatyage@gmail.com>
2015-09-30 11:02:09 +0900

    new hit form

--- app/components/app.js
+++ app/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Config from './config';
+import NewHITForm from './new-hit-form';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Config></Config>
+        <NewHITForm></NewHITForm>
       </div>
     );
   }
---
+++ app/components/new-hit-form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class NewHITForm extends React.Component {
+  render() {
+    return (
+      <div id="new-hit-form">
+        <div>
+          <label>HITテンプレート</label>
+          <textarea></textarea>
+        </div>
+      </div>
+    );
+  }
+}
