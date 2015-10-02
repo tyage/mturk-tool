2ae82991947a6e7648012129c0552be843fb43b4
tyage <namatyage@gmail.com>
2015-10-02 15:00:03 +0900

    response on init

--- server/choice-control.js
+++ server/choice-control.js
@@ -7,17 +7,21 @@ var choices = [];
 
 router.post('/init', function(req, res, next) {
   choices = req.body;
+
+  res.json({ 'success': true })
 });
 
 router.get('/get/:size', function(req, res, next) {
   var size = +req.params.size;
   var returnChoices = choices.slice(0, size);
   choices = choices.slice(size);
+
   res.json(returnChoices);
 });
 
 router.post('/return', function(req, res, next) {
   choices = choices.concat(req.body);
+
   res.json({ 'success': true })
 });
 
