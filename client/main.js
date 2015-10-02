af496a138f21dc5ce996948cd4c10a9f8b368a47
tyage <namatyage@gmail.com>
2015-10-02 13:18:18 +0900

    /client/app/

--- client/main.js
+++ client/main.js
@@ -19,7 +19,7 @@ app.on('ready', function() {
 
   // ブラウザ(Chromium)の起動, 初期画面のロード
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://' + __dirname + '/client/index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 
   mainWindow.on('closed', function() {
     mainWindow = null;
