763ba83a403af5e23a9169534096c1db1794f208
tyage <namatyage@gmail.com>
2015-11-10 17:29:03 +0900

    implement server

--- resources/src/js/app.js
+++ resources/src/js/app.js
@@ -5,12 +5,12 @@ let socket = io(window.questionControllerServer);
 // decompose url parameter
 // https://www.mturkcontent.com/dynamic/hit?assignmentId=${assignmentId}&hitId=${hitId}
 let params = {};
-window.location.search.slice(1).split('&').forEach((param) => {
+window.location.search.slice(1).split('&').forEach(param => {
   let [key, value] = param.split('=');
   params[key] = value;
 });
-socket.emit('requestHITContent', params);
+socket.emit('requestContent', params.hitId, params.assignmentId);
 
-socket.on('setHITContent', (data) => {
-  document.getElementById('content').innerHTML = data.content;
+socket.on('setContent', (hitId, content) => {
+  document.getElementById('content').innerHTML = content;
 });
--- server/app.js
+++ server/app.js
@@ -2,9 +2,30 @@
 
 let io = require('socket.io')(9290);
 
+let hitRequester = {};
+let hitWorker = {};
+
 io.on('connection', socket => {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  // requester wait until hit is assigned to worker
+  socket.on('waitAssignment', hitId => {
+    hitRequester[hitId] = socket;
+  });
+
+  // worker need HIT content
+  socket.on('requestContent', (hitId, assignmentId) => {
+    let requester = hitRequester[hitId];
+    hitWorker[hitId] = socket;
+    if (requester) {
+      // request HIT content to requester
+      requester.emit('requestContent', hitId, assignmentId);
+    }
+  });
+
+  // requester response HIT content
+  socket.on('setContent', (hitId, content) => {
+    let worker = hitWorker[hitId];
+    if (worker) {
+      worker.emit('setContent', hitId, content);
+    }
   });
 });
