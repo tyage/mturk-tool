a4ae414fd435d25685f46b1a8ebc2697940f4c49
tyage <namatyage@gmail.com>
2015-11-10 17:03:32 +0900

    send param and set content via websocket

--- resources/src/js/app.js
+++ resources/src/js/app.js
@@ -1,3 +1,16 @@
 import io from 'socket.io-client';
 
-io(window.questionControllerServer)
+let socket = io(window.questionControllerServer);
+
+// decompose url parameter
+// https://www.mturkcontent.com/dynamic/hit?assignmentId=${assignmentId}&hitId=${hitId}
+let params = {};
+window.location.search.slice(1).split('&').forEach((param) => {
+  let [key, value] = param.split('=');
+  params[key] = value;
+});
+socket.emit('requestHITContent', params);
+
+socket.on('setHITContent', (data) => {
+  document.getElementById('content').innerHTML = data.content;
+});
