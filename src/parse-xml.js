5992911c4bcbdcda5e97d8debc45a4a2814e9913
tyage <namatyage@gmail.com>
2015-11-11 15:14:34 +0900

    use xmldom for DOMParser

--- src/parse-xml.js
+++ src/parse-xml.js
@@ -1,4 +1,6 @@
-let domParser = new DOMParser()
+import { DOMParser } from 'xmldom';
+
+let domParser = new DOMParser();
 let parseXML = xml => domParser.parseFromString(xml, 'text/xml');
 
 export default parseXML;
