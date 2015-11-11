c1516814d603e2dbc895a7d97c67241aa8f755a0
tyage <namatyage@gmail.com>
2015-11-11 16:04:14 +0900

    debug log for server

--- server/app.js
+++ server/app.js
@@ -8,13 +8,16 @@ let hitWorker = {};
 io.on('connection', socket => {
   // requester wait until hit is assigned to worker
   socket.on('waitAssignment', hitId => {
+    console.log(`requester waiting for ${hitId}`);
+
     hitRequester[hitId] = socket;
   });
 
   // worker need HIT content
   socket.on('requestContent', (hitId, assignmentId) => {
+    console.log(`new worker is assigned to ${hitId} (assignmentId: ${assignmentId})`);
+
     let requester = hitRequester[hitId];
-    console.log(hitId)
     hitWorker[hitId] = socket;
     if (requester) {
       // request HIT content to requester
@@ -24,6 +27,8 @@ io.on('connection', socket => {
 
   // requester response HIT content
   socket.on('setContent', (hitId, content) => {
+    console.log(`requester response content to ${hitId}`);
+
     let worker = hitWorker[hitId];
     if (worker) {
       worker.emit('setContent', hitId, content);
