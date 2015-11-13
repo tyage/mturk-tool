01a7a32b269161d665d0b28b2dbc059478a2213c
tyage <namatyage@gmail.com>
2015-11-13 15:40:43 +0900

    use https server

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ let config = new Map();
 
 let defaultConfig = {
   apiEndpoint: 'https://mechanicalturk.sandbox.amazonaws.com', // default is sandbox!
-  questionControllerServer: 'http://mocos.kitchen' // call questionController.reconnect after change this
+  questionControllerServer: 'https://mocos.kitchen' // call questionController.reconnect after change this
 };
 for (let key in defaultConfig) {
   if (config.get(key) === undefined) {
