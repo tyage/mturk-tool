45c96aba0f80eb24caa6030452a6d2e520472986
tyage <namatyage@gmail.com>
2015-11-16 11:55:48 +0900

    implemet getAnswer method

--- src/assignment.js
+++ src/assignment.js
@@ -1,8 +1,42 @@
+import mturk from './mturk';
+
+let parseAssignment = assignment => {
+
+};
+
 export default class Assignment {
   constructor(assignment) {
     this.raw = assignment;
     this.worker = {
       id: 'test'
     };
+  }
+
+  getAnswer() {
+    // wait until the assignment status changes
+    let waitSecound = 30 * 1000;
+    let waitHIT = (resolve, reject) => {
+      mturk.getAssignment(this.id).then(result => {
+        let assignment = parseAssignment(result);
+        switch (assignment.AssignmentStatus) {
+          case 'Submitted':
+          case 'Approved':
+            resolve(assignment.answer);
+            break;
+          case 'Rejected':
+            reject();
+            break;
+          default:
+            global.setTimeout(() => waitHIT(resolve, reject), waitSecound);
+            break;
+        }
+      }).catch(() => {
+        global.setTimeout(() => waitHIT(resolve, reject), waitSecound);
+      });
+    };
+
+    return new Promise((resolve, reject) => {
+      waitHIT(resolve);
+    });
   }
 }
